8f92f9e14ad32206fd8b84b2570ceb269fa97914
HaT7 <tuanhagcd19902@fpt.edu.vn>
2023-12-05 12:25:17 +0700

    Fix sidebar profile

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,7 @@ const Employees = lazy(() => import('../pages/Management/Employees'));
 const Projects = lazy(() => import('../pages/Management/Projects'));
 const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
 const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
+const Profile = lazy(() => import('../pages/Profile'));
 const Settings = lazy(() => import('../pages/Settings'));
 const ChartProjects = lazy(() => import('../pages/Dashboard/ChartProjects'));
 const ChartEmployees = lazy(() => import('../pages/Dashboard/ChartEmployees'));
@@ -37,6 +38,11 @@ const coreRoutes = [
     path: '/ui/buttons',
     title: 'Buttons',
     component: Buttons,
+  },
+  {
+    path: '/profile',
+    title: 'Profile',
+    component: Profile,
   },
   {
     path: '/settings',
