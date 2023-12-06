0483efe7a7570dd2099235653eb4fa77bbd375db
Phuc <phuc05032001@outlook.com>
2023-12-06 11:27:07 +0700

    FP-21 table ui

---
+++ src/pages/Management/Employees/Skills/makeData.ts
@@ -0,0 +1,51 @@
+export type User = {
+  id: string
+  name: string
+  year: string
+}
+
+export const fakeData: User[] = [
+  {
+    id: '9s41rp',
+    name: 'Kelvin',
+    year: '1'
+  },
+  {
+    id: '08m6rx',
+    name: 'Molly',
+    year: '1'
+  },
+  {
+    id: '5ymtrc',
+    name: 'Henry',
+    year: '1'
+  },
+  {
+    id: 'ek5b97',
+    name: 'Glenda',
+    year: '1'
+  },
+  {
+    id: 'xxtydd',
+    name: 'Leone',
+    year: '1'
+  },
+  {
+    id: 'wzxj9m',
+    name: 'Mckenna',
+    year: '1'
+  },
+  {
+    id: '21dwtz',
+    name: 'Wyman',
+    year: '1'
+  },
+  {
+    id: 'o8oe4k',
+    name: 'Janick',
+    year: '1'
+  }
+]
+
+//50 us states array
+export const usYears = ['1', '2', '3']
