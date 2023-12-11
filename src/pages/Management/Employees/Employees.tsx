56cb63f90d639d4bfb230cb7a67752686159b4c1
nguyentranazz123 <nguyenttgcd210485@fpt.edu.vn>
2023-12-11 11:03:52 +0700

    FEAT employeelist
    update FEAT employee list
    
    FEAT project list
    
    ":wq

--- src/pages/Management/Employees/Employees.tsx
+++ src/pages/Management/Employees/Employees.tsx
@@ -1,41 +1,121 @@
-import { useState } from 'react';
-import Breadcrumb from '../../../components/Breadcrumb';
-import { Button } from '@mui/material';
-import AddIcon from '@mui/icons-material/Add';
-import CreateEmployeeModal from './Create';
 
-const Employees = () => {
-  const [visibleModalAddUpdate, setVisibleModalAddUpdate] = useState<boolean>(false);
+import { useMemo, useState, useEffect } from 'react';
+import { MaterialReactTable, useMaterialReactTable, type MRT_ColumnDef, MRT_Row } from 'material-react-table';
+import { Box, IconButton, Tooltip } from '@mui/material';
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
+import axios from 'axios';
 
-  const handleCloseModalAddUpdate = () => {
-    setVisibleModalAddUpdate(false);
+interface Skill {
+  exp: string;
+  name: string;
+}
+interface Person {
+  id: number;
+  name: string;
+  phone: string;
+  date_of_birth: string;
+  skills: Skill[];
+  
+}
+
+const EmployeesList = () => {
+  const [data, setData] = useState<Person[]>([]);
+
+  // Fetch data from your API when the component mounts
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://hrm-server-api.onrender.com/api/employees');
+    setData(response.data.data);
+    console.log(response.data.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
   };
 
-  const handleOpenModalAddUpdate = () => {
-    setVisibleModalAddUpdate(true);
-  };
+  // Columns definition
+  const columns = useMemo<MRT_ColumnDef<Person>[]>(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+        size: 100,
+      },
+      {
+        accessorKey: 'name',
+        header: 'Name',
+        size: 100,
+      },
+      {
+        accessorKey: 'phone',
+        header: 'Phone Number',
+        size: 100,
+      },  {
+        accessorKey: 'date_of_birth',
+        header: 'Date of Birth',
+        size: 100,
+        Cell: ({ row }) => new Date(row.original.date_of_birth).toLocaleDateString(),
+      },  {
+        accessorKey: 'skills[name]',
+        header: 'Skill',
+        size: 100,
+        Cell: ({ row }) => (
+          <ul>
+            {row.original.skills.map((skill: Skill) => (
+              <li key={skill.name}>{skill.name} - {skill.exp} years</li>
+             
 
-  return (
-    <>
-      <Breadcrumb pageName='Employees' />
-      <Button
-        size='medium'
-        type='button'
-        style={{ margin: '1rem 0' }}
-        variant='contained'
-        startIcon={<AddIcon />}
-        onClick={handleOpenModalAddUpdate}
-      >
-        Create new Employee
-      </Button>
-
-      {visibleModalAddUpdate && (
-        <CreateEmployeeModal visible={visibleModalAddUpdate} onClose={handleCloseModalAddUpdate} initialValue={{}} />
-      )}
-
-      
-    </>
+            ))}
+          </ul>
+        ),
+      },  
+    ],
+    [],
   );
+
+  // DELETE action
+  const deleteUser = async (id: number) => {
+    try {
+      await axios.delete(`https://hrm-server-api.onrender.com/api/employees/${id}`);
+      fetchData(); // Fetch updated data after deletion
+    } catch (error) {
+      console.error('Error deleting user:', error);
+    }
+  };
+
+  const openDeleteConfirmModal = (row: MRT_Row<Person>) => {
+    if (window.confirm('Are you sure you want to delete this user?')) {
+      deleteUser(row.original.id);
+    }
+  };
+
+  const table = useMaterialReactTable({
+    columns,
+    data,
+    editDisplayMode: 'modal',
+    enableEditing: true,
+    positionActionsColumn: 'last',
+    renderRowActions: ({ row, table }) => (
+      <Box sx={{ display: 'flex', gap: '.5em' }}>
+        <Tooltip title="Edit">
+          <IconButton onClick={() => table.setEditingRow(row)}>
+            <EditIcon />
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete">
+          <IconButton color="error" onClick={() => openDeleteConfirmModal(row)}>
+            <DeleteIcon />
+          </IconButton>
+        </Tooltip>
+      </Box>
+    ),
+  });
+
+  return <MaterialReactTable table={table}  />;
 };
 
-export default Employees;
+export default EmployeesList;
