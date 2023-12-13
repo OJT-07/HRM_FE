f5deab52932b5d14a95814bedf33524f40d01f86
Phuc <phuc05032001@outlook.com>
2023-12-13 10:17:14 +0700

    fix: add route for employee  detail

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,55 +1,60 @@
-import { lazy } from 'react';
-const Employees = lazy(() => import('../pages/Management/Employees'));
-const Projects = lazy(() => import('../pages/Management/Projects'));
-const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
-const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
-const Profile = lazy(() => import('../pages/Profile'));
-const Settings = lazy(() => import('../pages/Settings'));
-const ChartProjects = lazy(() => import('../pages/Dashboard/ChartProjects'));
-const ChartEmployees = lazy(() => import('../pages/Dashboard/ChartEmployees'));
-
+import { lazy } from 'react'
+const Employees = lazy(() => import('../pages/Management/Employees'))
+const Projects = lazy(() => import('../pages/Management/Projects'))
+const Alerts = lazy(() => import('../pages/UiElements/Alerts'))
+const Buttons = lazy(() => import('../pages/UiElements/Buttons'))
+const Profile = lazy(() => import('../pages/Profile'))
+const Settings = lazy(() => import('../pages/Settings'))
+const ChartProjects = lazy(() => import('../pages/Dashboard/ChartProjects'))
+const ChartEmployees = lazy(() => import('../pages/Dashboard/ChartEmployees'))
+const RenderInformation = lazy(() => import('../pages/Management/Employees/Details'))
 const coreRoutes = [
   {
     path: '/chart-employees',
     title: 'Chart Employees',
-    component: ChartEmployees,
+    component: ChartEmployees
   },
   {
     path: '/chart-projets',
     title: 'Chart Projects',
-    component: ChartProjects,
+    component: ChartProjects
   },
   {
     path: '/management/employees',
     title: 'Employees Management',
-    component: Employees,
+    component: Employees
   },
   {
     path: '/management/projects',
     title: 'Projects Management',
-    component: Projects,
-  },  
+    component: Projects
+  },
   {
     path: '/ui/alerts',
     title: 'Alerts',
-    component: Alerts,
+    component: Alerts
   },
   {
     path: '/ui/buttons',
     title: 'Buttons',
-    component: Buttons,
+    component: Buttons
   },
   {
     path: '/profile',
     title: 'Profile',
-    component: Profile,
+    component: Profile
   },
   {
     path: '/settings',
     title: 'Settings',
-    component: Settings,
+    component: Settings
   },
-];
+  {
+    path: '/management/employees/:id/detail',
+    title: 'Employees Management',
+    component: RenderInformation
+  }
+]
 
-const routes = [...coreRoutes];
-export default routes;
+const routes = [...coreRoutes]
+export default routes
