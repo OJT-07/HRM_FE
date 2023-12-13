9451a300cd2f286ca0415bab33e4a06805735562
nguyentranazz123 <nguyenttgcd210485@fpt.edu.vn>
2023-12-13 14:40:12 +0700

    update create button

--- src/pages/Management/Projects/Projects.tsx
+++ src/pages/Management/Projects/Projects.tsx
@@ -42,7 +42,7 @@ const EmployeesList = () => {
     setData(response.data.data);
     console.log(response.data.data);
     } catch (error) {
-      console.error('Error fetching data:', error);
+      console.error('Errors fetching data:', error);
     }
   };
 
