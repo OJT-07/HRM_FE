4f457049d982c6da0d04f0fc6be8987388098db7
lamvbg <lamvbgcd191320@fpt.edu.vn>
2023-12-15 14:31:22 +0700

    push routeIndex

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
-import { lazy } from 'react'
-const Employees = lazy(() => import('../pages/Management/Employees/Employees'))
-const Projects = lazy(() => import('../pages/Management/Projects/Projects'))
-const detailsProjects = lazy(() => import('../pages/Management/Projects/Details'))
-=======
 import { lazy } from 'react';
 const Employees = lazy(() => import('../pages/Management/Employees/Employees'));
 const Projects = lazy(() => import('../pages/Management/Projects/Projects'));
 const detailsProjects = lazy(() => import('../pages/Management/Projects/Details'));
->>>>>>> feat/FP-26-Update-employee
 // const detailsEmployee = lazy(() => import('../pages/Management/Employee/employeeDetails'))
 const Alerts = lazy(() => import('../pages/UiElements/Alerts'));
 const Buttons = lazy(() => import('../pages/UiElements/Buttons'));
@@ -17,6 +10,7 @@ const Settings = lazy(() => import('../pages/Settings'));
 const ChartProjects = lazy(() => import('../pages/Dashboard/ChartProjects'));
 const ChartEmployees = lazy(() => import('../pages/Dashboard/ChartEmployees'));
 const RenderInformation = lazy(() => import('../pages/Management/Employees/Details'));
+const ExportCV = lazy(() => import('../pages/Management/Projects/ExportCV'))
 const coreRoutes = [
   {
     path: '/chart-employees',
@@ -74,6 +68,11 @@ const coreRoutes = [
     path: '/management/employees/:id/detail',
     title: 'Employees Management',
     component: RenderInformation
+  },
+  {
+    path: '/managemet/exportCV',
+    title: 'Export CV',
+    component: ExportCV
   }
 ];
 
