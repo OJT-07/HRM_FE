93c178d0cb6bf3713e9375529f8b6ef79b24d81f
Quy Nguyen <quy.nguyen@devplus.edu.vn>
2023-12-21 09:29:25 +0700

    devploy

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import pluginRewriteAll from 'vite-plugin-rewrite-all';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    watch: {
-      usePolling: true
-    }
+  plugins: [pluginRewriteAll(), react()],
+  build: {
+    outDir: 'build'
   }
-})
+});
